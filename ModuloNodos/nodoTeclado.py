b774b10af80ba745c3ee1fe49767163a9235fd42
AlvaroR4 <arv4en2@gmail.com>
2025-10-13 18:44:31 +0200

    -

--- ModuloNodos/nodoTeclado.py
+++ ModuloNodos/nodoTeclado.py
@@ -75,62 +75,80 @@ class NodoTeclado(Node):
 
     def _procesar_tecla(self, ch):
         ch = ch.lower()
-        nuevo = [0.0, 0.0, 0.0, 0.0]
+        nuevo = self.comando_actual[:] # Empezamos con el comando actual
+        nombre = ""
+
+        # Comandos que establecen una velocidad continua
         if ch == 'w':
             self.avance += VELOCIDAD_AVANCE
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]    
             nombre = "AVANZAR (w)"
         elif ch == 's':
             self.avance -= VELOCIDAD_AVANCE
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "ATRÁS (s)"
         elif ch == 'a':
             self.lateral -= VELOCIDAD_LATERAL
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "IZQUIERDA (a)"
         elif ch == 'd':
             self.lateral += VELOCIDAD_LATERAL
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "DERECHA (d)"
         elif ch == 'q':
-            self.yaw -=VELOCIDAD_YAW
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
+            self.yaw -= VELOCIDAD_YAW
             nombre = "GIRAR IZQUIERDA (q)"
         elif ch == 'e':
             self.yaw += VELOCIDAD_YAW
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "GIRAR DERECHA (e)"
         elif ch == 'y':
             self.vertical += VELOCIDAD_VERTICAL
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "SUBIR (y)"
         elif ch == 'h':
             self.vertical -= VELOCIDAD_VERTICAL
-            nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
             nombre = "BAJAR (h)"
         elif ch == ' ':
             self.avance = 0.0
             self.vertical = 0.0
             self.lateral = 0.0
             self.yaw = 0.0
-            nuevo = [self.vertical, self.avance, self.yaw, self.vertical]
             nombre = "PARAR (space)"
-        elif ch == 'l':
-            nuevo = [2.0, 0.0, 0.0, 0.0] #se interpreta como land
-            nombre = "LAND (l)" 
-        elif ch == 'o':
-            nuevo = [2.0, 2.0, 0.0, 0.0] #se interpreta como emergency
-            nombre = "EMERGENCY (o)" 
-        elif ch == 't':
-            nuevo = [2.0, 2.0, 2.0, 0.0] #se interpreta como takeoff
-            nombre = "TAKEOFF (t)" 
-        else:
+        
+        # Comandos especiales que se publican una vez y luego paran
+        elif ch in ['l', 'o', 't']:
+            comando_especial = [0.0, 0.0, 0.0, 0.0]
+            if ch == 'l':
+                comando_especial = [2.0, 0.0, 0.0, 0.0] # se interpreta como land
+                nombre = "LAND (l)"
+            elif ch == 'o':
+                comando_especial = [2.0, 2.0, 0.0, 0.0] # se interpreta como emergency
+                nombre = "EMERGENCY (o)"
+            elif ch == 't':
+                comando_especial = [2.0, 2.0, 2.0, 0.0] # se interpreta como takeoff
+                nombre = "TAKEOFF (t)"
+
+            # Publica el comando inmediatamente
+            msg = Float32MultiArray()
+            msg.data = [float(val) for val in comando_especial]
+            self.pub.publish(msg)
+            self.get_logger().info(f"Comando enviado: {nombre}")
+
+            self.avance = 0.0
+            self.vertical = 0.0
+            self.lateral = 0.0
+            self.yaw = 0.0
+            
+            nuevo = [0.0, 0.0, 0.0, 0.0]
+            with self._lock:
+                self.comando_actual = nuevo
             return
+
+        else:
+            return # Tecla no reconocida
+
+        nuevo = [self.lateral, self.avance, self.vertical, self.yaw]
 
         with self._lock:
             if any(abs(a - b) > 1e-6 for a, b in zip(self.comando_actual, nuevo)):
-                self.get_logger().info(f"Comando activo: {nombre} .VELOCIDAD = {self.lateral}, {self.avance}, {self.yaw}, {self.vertical}")
+                self.get_logger().info(f"Comando activo: {nombre} .VELOCIDAD = {[self.lateral, self.avance, self.vertical, self.yaw]}")
             self.comando_actual = nuevo
+
 
     def destroy_node(self):
         self._running = False
